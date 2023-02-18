f122d4b7c869b998fa260fa27a3038662f0ce473
Argmaster <argmaster.world@gmail.com>
2023-02-18 01:09:58 +0100

    Fix _create_file_handler not relying on log_dir parameter

--- log.py
+++ log.py
@@ -168,8 +168,8 @@ def _create_file_handler(
     max_log_files: int,
 ) -> logging.Handler:
     # Create logging directory before trying to put files there.
-    LOG_DIR.mkdir(0o777, True, True)
-    log_file = LOG_DIR / logger_name
+    log_dir.mkdir(0o777, True, True)
+    log_file = log_dir / logger_name
     # If file already exists, logger may decide to start appending its logs to it,
     # which is not desired - preferably, one file for one session.
     log_file_already_exists = log_file.exists()
